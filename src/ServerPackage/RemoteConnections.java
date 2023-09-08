c7e801b3a2c921a34e7e97ea40b6558eebc759f5
the-pacifist-3 <rrohith958@gmail.com>
2023-09-08 15:49:50 +0530

    Creation and destruction of sockets within a class

--- src/ServerPackage/RemoteConnections.java
+++ src/ServerPackage/RemoteConnections.java
@@ -1,5 +1,31 @@
 package ServerPackage;
+import java.net.*;
+
 
 public class RemoteConnections {
-
+	public ServerSocket startSockets(int port) {
+		try {
+			ServerSocket ss = new ServerSocket(port);
+			ss.accept();
+			System.out.println("Listening on 127.0.0.1:"+port+"........");
+			return ss;
+		}catch(Exception e) {
+			System.out.println("##--Error in Binding to port "+port+"--##\n"+e);
+			return null;
+		}
+	}public int stopSockets(ServerSocket ss) {
+		try {
+			if(ss.isClosed()) {
+				System.out.println("##--Port already Closed--##");
+				return 1;
+			}else {
+				System.out.println("Closing Port"+ss.getLocalPort()+"\n");
+				ss.close();
+				return 1;
+			}
+		}catch(Exception e) {
+			System.out.println("##--Error while closing port "+ss.getLocalPort()+"--##\n"+e);
+			return -1;
+		}
+	}
 }
